7c002f70aa1bcc6ddfea427815350049668fb6eb
EdViaz <edoardo.viale.06@gmail.com>
2024-05-16 08:24:37 +0200

    merge sort

--- appUwU/src/views/Account.vue
+++ appUwU/src/views/Account.vue
@@ -1,13 +1,27 @@
 <template>
     <section class="section">
         <div class="container">
-            <h1 class="title">suca devo ancora pensarci</h1>
-            
+            <h1 class="title">* Indirizzo mail *</h1>
+            <div class="boxerellino">
+                <div class="container">
+                    <h2 class="title">Giocate recenti</h2> <br>
+                    * inserire lista ultime bho 5 giocate * <br>
+                    <h3 class="title">Vincita massima</h3> <br>
+                    * Inserire giocata king oriaghica per invogliare il gambling even more *
+                </div>
+                <div class="container">
+                    <h2 class="title" >Saldo attuale</h2> <br>
+                    * saldo saldo uwu saldo* <br>
+                    <h3 class="title">Massimo storico</h3> <br>
+                    * top 3 lupi di wolf street *
+                </div>
+            </div>
         </div>
     </section>
 </template>
 
 <script>
+
 export default {
     name: 'Account',
     data() {
@@ -21,4 +35,16 @@ export default {
 </script>
 
 <style scoped>
+
+.boxerellino{
+    display: flex;
+    justify-content: space-evenly;
+}
+h3{
+    font-size: medium;
+}
+h2{
+    background-color: aquamarine;
+    color: black;
+}
 </style>
