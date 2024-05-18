c364750047b183a5187d17bdd4b9f8193c5a68f3
EdViaz <edoardo.viale.06@gmail.com>
2024-05-18 15:00:18 +0200

    omg bro look at the animated title!😎

--- appUwU/src/main.js
+++ appUwU/src/main.js
@@ -4,6 +4,7 @@ import router from './router';
 import 'bulma/css/bulma.css'
 //import '../src/style.css';
 
+
 import VueSweetalert2 from 'vue-sweetalert2';
 import 'sweetalert2/dist/sweetalert2.min.css';
 
@@ -14,3 +15,22 @@ app.use(VueSweetalert2);
 app.use(router);
 
 app.mount('#app');
+
+let originalTitle = document.title;
+let titleInterval;
+
+function scrollTitle() {
+    const title = document.title;
+    const newTitle = title.substring(1) + title.charAt(0);
+    document.title = newTitle;
+}
+
+window.addEventListener('blur', function () {
+    document.title = 'Torna😭🙏';
+    clearInterval(titleInterval); // clear the interval when the page loses focus
+});
+
+window.addEventListener('focus', function () {
+    document.title = originalTitle;
+    titleInterval = setInterval(scrollTitle, 500); // set the interval when the page gains focus
+});
